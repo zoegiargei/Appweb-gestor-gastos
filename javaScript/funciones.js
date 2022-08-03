bf68f5f80d0dab9b7be175fa8b4b82f501ddb5d9
Zoe <mariagiargei031@gmail.com>
2022-08-02 23:23:48 -0300

    apliqué destructuring + operador ternario

--- javaScript/funciones.js
+++ javaScript/funciones.js
@@ -1,24 +1,62 @@
+function mostrarAlerta(){
+    divMostrarPresupuesto.classList.toggle(`presupuestoEnRojo`)
+    divMostrarPresupuesto.classList.toggle(`vibrate-1`)
+    divMostrarPresupuesto.classList.remove(`presupuestoNormal`)
+}
+
+function quitarAlerta(){
+    divMostrarPresupuesto.classList.remove(`presupuestoEnRojo`)
+    divMostrarPresupuesto.classList.remove(`vibrate-1`)
+}
+
+/*
+function eliminarGasto(){
+
+    let datosEnStorage = JSON.parse(localStorage.getItem(`gastos`))
+    let presupuestoEnStorage = JSON.parse(localStorage.getItem(`presupuesto`))
+
+    datosEnStorage.forEach((gasto, indice) => {
+
+        const botonGastoAEliminar = document.getElementById(`gasto${indice}`).lastElementChild.lastElementChild
+        botonGastoAEliminar.addEventListener("click", () =>{
+            document.getElementById(`gasto${indice}`).remove()
+            datosEnStorage.splice(datosEnStorage.indexOf(gasto), 1)
+            localStorage.setItem("gastos", JSON.stringify(datosEnStorage))
+
+            presupuestoEnStorage.presupuesto += gasto.monto
+            localStorage.setItem(`presupuesto`, JSON.stringify(presupuestoEnStorage))
+            mostrarPresupuesto(presupuesto)
+        })
+    })
+
+}
+*/
+
 //Función para mostrar prespuesto en DOM
 function mostrarPresupuesto(presupuesto){
 
-    //Aplicar acá método Destructuring
-
     let presupuestoEnStorage = JSON.parse(localStorage.getItem(`presupuesto`)) //Pedimos VALUE del storage con GET + KEY(CLAVE)
 
+
+    if(presupuestoEnStorage.presupuesto > 10 && divMostrarPresupuesto.classList.contains(`presupuestoEnRojo`)){
+        quitarAlerta()
+
+        if((divMostrarPresupuesto.classList.contains(`presupuestoNormal`) != true)){
+            divMostrarPresupuesto.classList.toggle(`presupuestoNormal`)
+        }
+    }
+    
     divMostrarPresupuesto.innerHTML = ``
 
     divMostrarPresupuesto.innerHTML += `
         <p>Presupuesto: ${presupuestoEnStorage.presupuesto} ${presupuestoEnStorage.divisa}</p>
     `
 
-    if (presupuestoEnStorage < 10){
-        divMostrarPresupuesto.classList.toggle(`presupuestoEnRojo`)
-        divMostrarPresupuesto.classList.toggle(`vibrate-1`)
-        divMostrarPresupuesto.classList.remove(`presupuestoNormal`)
-    }
+    //Aplicación de operador Ternario.
+    presupuestoEnStorage.presupuesto < 10 ? mostrarAlerta() : quitarAlerta()
 }
 
-//función para mostrar gastos
+//función para mostrar gastos en DOM
 function mostrarGastos(){
 
     let datosEnStorage = JSON.parse(localStorage.getItem(`gastos`))
@@ -37,38 +75,45 @@ function mostrarGastos(){
                 <button class="btnEliminarGasto">Eliminar Gasto</button>
             </div>
         </div>
-        
         `
 
-        datosEnStorage.forEach(() => {
-    
-            let btnEliminarGasto = document.getElementById(`gasto${indice}`).lastElementChild.lastElementChild
-            let presupuestoEnStorage = JSON.parse(localStorage.getItem(`presupuesto`))
-    
-            btnEliminarGasto.addEventListener(`click`, () => {
-                
-                document.getElementById(`gasto${indice}`).remove()
+        let btnEliminarGasto = document.getElementById(`gasto${indice}`).lastElementChild.lastElementChild
+        let presupuestoEnStorage = JSON.parse(localStorage.getItem(`presupuesto`))
+
+        btnEliminarGasto.addEventListener(`click`, () => {
+
+            divGastosFiltrados.innerHTML = ``
+            
+            document.getElementById(`gasto${indice}`).remove()
+            gastos.splice(indice, 1)
+
+            if(gastos.length == 1){
+                let gastoAEliminar = document.getElementById(`gasto${0}`) 
+                gastoAEliminar.remove()
+                gastos.shift()
+            } else{
+                let gastoAEliminar = document.getElementById(`gasto${indice}`)
+                gastoAEliminar.remove()
                 gastos.splice(indice, 1)
-                
-                presupuestoEnStorage.presupuesto += parseFloat(gasto.monto)
-                //Pruebas en consola
-                console.log(`Monto de gasto a eliminar: ${gasto.monto}`)
-                console.log(`presupuesto + gasto eliminado: ${presupuestoEnStorage.presupuesto}`)
-                //
-                
-                if(presupuesto>10 && divMostrarPresupuesto.classList.contains(`presupuestoEnRojo`)){
-                    
-                    divMostrarPresupuesto.classList.remove(`presupuestoEnRojo`)
-                    divMostrarPresupuesto.classList.remove(`vibrate-1`)
-                    divMostrarPresupuesto.classList.toggle(`presupuestoNormal`)
-                }
-                
-                localStorage.setItem(`presupuesto`, JSON.stringify(presupuestoEnStorage))
-                localStorage.setItem(`gastos`, JSON.stringify(gastos))
-                console.log(`${gasto.titulo} Eliminado`)
-                
-                mostrarPresupuesto(presupuesto)
-            })
+            }
+            
+            //Aplicación de Destructuring
+            let {monto} = gasto
+
+            console.log(`Presupuesto antes de sumar gasto eliminado: ${presupuestoEnStorage.presupuesto}`) //Prueba en consola
+
+            presupuestoEnStorage.presupuesto += parseFloat(monto)
+
+            //Pruebas en consola
+            console.log(`Monto de gasto a eliminar: ${monto}`)
+            console.log(`presupuesto + gasto eliminado: ${presupuestoEnStorage.presupuesto}`)
+            //
+            
+            localStorage.setItem(`presupuesto`, JSON.stringify(presupuestoEnStorage))
+            localStorage.setItem(`gastos`, JSON.stringify(gastos))
+            console.log(`${gasto.titulo} Eliminado`)
+            
+            mostrarPresupuesto(presupuesto)
         })
     })
 }
--- javaScript/scripts.js
+++ javaScript/scripts.js
@@ -1,7 +1,7 @@
 //Array de gastos del usuario
-let gastos = []
+//let gastos = []
 //Presupuesto y divisa
-let presupuesto = {}
+//let presupuesto = {}
 
 //Variables capturadas del DOM
 const formPresupuesto = document.getElementById(`formPresupuesto`)
@@ -13,15 +13,16 @@ const btnMostrarPresupuesto = document.getElementById(`btnMostrarPresupuesto`)
 const divPresupuestoAgotado = document.getElementById(`divPresupuestoAgotado`)
 const divValidacionPresupuesto = document.getElementById(`divValidacionPresupuesto`)
 
+//Aplicación de operador Lógico Or
+let gastos = JSON.parse(localStorage.getItem(`gastos`)) || []
+let presupuesto = JSON.parse(localStorage.getItem(`presupuesto`)) || {}
 
 //Preguntamos en el Local storage si hay guardados datos de Gastos, Presupuesto y Divisa
-if (localStorage.getItem(`gastos`,`presupuesto`,`divisa`)) { //consulto y capturo datos del storage
-    
-    gastos = JSON.parse(localStorage.getItem(`gastos`))
-    presupuesto = JSON.parse(localStorage.getItem(`presupuesto`))
-    
+if (localStorage.getItem(`gastos`,`presupuesto`)) { //consulto y capturo datos del storage
+
     divContenedorPresupuesto.classList.toggle(`removeContenedor`)
     divAgregarGastos.classList.remove(`display`)
+    mostrarPresupuesto(presupuesto)
     
 } else { //Si la respuesta es que NO hay datos guardados agregamos al local storage las variables iniciadas vacías
 
@@ -59,7 +60,7 @@ formGastos.addEventListener(`submit`, (e) => {
 
     let datForm = new FormData(e.target) /*Usamos el objeto formData; para poder capturar los datos del DOM con formData debemos agregar atribuo name a los elementos que estén dentro del formulario*/
     
-    let gasto = new Gasto(datForm.get(`titulo`), datForm.get(`categoria`), datForm.get(`monto`))
+    let gasto = new Gasto(datForm.get(`titulo`), datForm.get(`categoria`), Number(datForm.get(`monto`)))
     
     let presupuestoEnStorage = JSON.parse(localStorage.getItem(`presupuesto`))
 
@@ -90,9 +91,135 @@ formGastos.addEventListener(`submit`, (e) => {
 })
 
 const btnGastos = document.getElementById(`btnGastos`)
-const divGastos = document.getElementById(`divGastos`)
 
 btnGastos.addEventListener(`click`, () => {
     mostrarGastos()
 })
+
+//Filtrados
+const formFiltros = document.getElementById(`formFiltros`)
+const divGastosFiltrados = document.getElementById(`divGastosFiltrados`)
+
+formFiltros.addEventListener(`submit`, (e) => {
+    e.preventDefault()
+
+    const filtros = document.getElementById(`filtros`).value
+    let datosEnStorage = JSON.parse(localStorage.getItem(`gastos`))
+
+    if(filtros == `Categoría`){
+
+
+        divGastosFiltrados.innerHTML = ``
+        divGastosFiltrados.innerHTML += `
+            <select id="categoriaFiltrar" name="categoriaFiltrar" class="form-select form-select-sm mt-3" aria-label=".form-select-sm example">
+                <option selected>Elija Categoría de Gastos que desea ver</option>
+                <option value="Alimentación">Alimentación</option>
+                <option value="Cuidado Personal">Cuidado Personal</option>
+                <option value="Entretenimiento y Salidas">Entretenimiento y Salidas</option>
+                <option value="Indumentaria">Indumentaria</option>
+                <option value="Salud">Salud</option>
+                <option value="Vivienda">Vivienda</option>
+                <option value="Limpieza">Limpieza</option>
+                <option value="Mascota">Mascota</option>
+                <option value="Impuestos">Impuestos</option>
+                <option value="Servicios">Servicios</option>
+                <option value="Viáticos">Viáticos</option>
+                <option value="Viaje">Viaje</option>
+                <option value="Extras">Extras</option>
+            </select>
+            <button id="btnCategoria" class="btn btn-primary mt-1 mb-3">Elejir</button>
+        `
+
+        document.getElementById(`btnCategoria`).addEventListener(`click`, ()=>{
+
+            let existe = datosEnStorage.some(gasto => gasto.categoria == document.getElementById(`categoriaFiltrar`).value)
+            console.log(existe)
+            if(existe){
+                
+                let gastosCategoria = datosEnStorage.filter(gasto => gasto.categoria == document.getElementById(`categoriaFiltrar`).value)
+
+                gastosCategoria.forEach((gasto) => {
+
+                    divGastosFiltrados.innerHTML += `
+                    <div class="mt-3 contenedorMostrarGasto" style="max-width: 20rem; margin:4px;">
+                        <div><h4>${gasto.titulo}</h4></div>
+                        <div>
+                            <p>Categoría: ${gasto.categoria}</p>
+                            <p>Monto: ${gasto.monto} ${presupuesto.divisa}</p>
+                            <p>Fecha: ${gasto.fecha}</p>
+                        </div>
+                    </div>
+                `
+                })
+            } else{
+
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'No existe gasto con esa categoría!',
+                })
+            }
+        })
+
+    } else if(filtros == `MenorAMayor`){
+        divGastosFiltrados.innerHTML = ``
+        let datosEnStorage = JSON.parse(localStorage.getItem(`gastos`))
+
+        datosEnStorage.sort(function (a, b) {
+            if (a.monto > b.monto) {
+                return 1;
+            }
+            if (a.monto < b.monto) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        })
+
+        datosEnStorage.forEach((gasto) => {
+
+            divGastosFiltrados.innerHTML += `
+            <div class="mt-3 contenedorMostrarGasto" style="max-width: 20rem; margin:4px;">
+                <div><h4>${gasto.titulo}</h4></div>
+                <div>
+                    <p>Categoría: ${gasto.categoria}</p>
+                    <p>Monto: ${gasto.monto} ${presupuesto.divisa}</p>
+                    <p>Fecha: ${gasto.fecha}</p>
+                </div>
+            </div>
+        `
+        })
+
+    }else if(filtros == `MayorAMenor`){
+        divGastosFiltrados.innerHTML = ``
+        let datosEnStorage = JSON.parse(localStorage.getItem(`gastos`))
+
+        datosEnStorage.sort(function (a, b) {
+            if (a.monto < b.monto) {
+                return 1;
+            }
+            if (a.monto > b.monto) {
+                return -1;
+            }
+            // a must be equal to b
+            return 0;
+        })
+
+        datosEnStorage.forEach((gasto) => {
+
+            divGastosFiltrados.innerHTML += `
+            <div class="mt-3 contenedorMostrarGasto" style="max-width: 20rem; margin:4px;">
+                <div><h4>${gasto.titulo}</h4></div>
+                <div>
+                    <p>Categoría: ${gasto.categoria}</p>
+                    <p>Monto: ${gasto.monto} ${presupuesto.divisa}</p>
+                    <p>Fecha: ${gasto.fecha}</p>
+                </div>
+            </div>
+        `
+        })
+
+    }
+})
+
 
